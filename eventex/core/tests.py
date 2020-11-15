2b97539f24bd02ac6dd44db77ad728220dcecd4d
Felipe Barros <felipe.b4rros@gmail.com>
2020-11-15 16:34:21 -0300

    add test home

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomTest(TestCase):
+    def setUp(self):
+        # preparacao de contexto
+        self.response = self.client.get('/')  # client realiza requisicoes dentro do django e esta simulando acesso a urls '/'
+        # usa-se o self para que ele se torne atributo da instancia e possa ser usado nos demias testes
+
+    def test_get(self):
+        """get / must return status code 200"""
+        self.assertEqual(200, self.response.status_code) # expectativa, status obtido
+
+    def test_template(self):
+        """must use index.hml"""
+        self.assertTemplateUsed(self.response, 'index.html')
