53926b633270f5a8049b666fc44c0e6d61bcdbe3
Felipe Barros <felipe.b4rros@gmail.com>
2020-11-26 16:29:10 -0300

    add e vincula model com form

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+             name="Felipe",
+             cpf='12345678901',
+             email='felipe.b4rros@gmail.com',
+             phone='21-9911-9933')
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created at attr."""
+        self.assertIsInstance(self.obj.created_at, datetime)
