29994f0b09311a4205cd9ad21e09d6757d83222b
Felipe Barros <felipe.b4rros@gmail.com>
2020-12-06 23:27:24 -0300

    implementa hash ao inves de pk

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -7,7 +7,7 @@ class Subscription(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
-    cpf_hash = models.IntegerField(null=True)
+    cpf_hash = models.IntegerField()
 
     class Meta:
         verbose_name_plural = 'Inscrições'
