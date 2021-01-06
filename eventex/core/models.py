6228d1653f5f20671e6a4bed0d7b46ed6299f24d
Felipe Barros <felipe.b4rros@gmail.com>
2021-01-05 23:05:16 -0300

    add talks

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -36,3 +36,16 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.value
+
+class Talk(models.Model):
+    title = models.CharField('Título', max_length=200)
+    start = models.TimeField('Início', blank=True, null=True)
+    description = models.TextField('Descrição', blank=True)
+    speakers = models.ManyToManyField('Speaker', verbose_name='Palestrantes', blank=True)
+
+    class Meta:
+        verbose_name = 'palestra'
+        verbose_name_plural = 'palestras'
+
+    def __str__(self):
+        return self.title
