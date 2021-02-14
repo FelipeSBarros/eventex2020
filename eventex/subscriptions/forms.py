a70d243104f051e68540bfa4bb8933bb86f2a911
Felipe Barros <felipe.b4rros@gmail.com>
2021-02-14 18:16:52 -0300

    clean code

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,25 +1,7 @@
 from django import forms
 from django.core.exceptions import ValidationError
+
 from eventex.subscriptions.models import Subscription
-from eventex.subscriptions.validators import validate_cpf
-
-
-class SubscriptionFormOld(forms.Form):
-    name = forms.CharField(label="Nome")
-    cpf = forms.CharField(label="CPF", validators=[validate_cpf])
-    email = forms.EmailField(label="E-mail", required=False)
-    phone = forms.CharField(label="Telefone", required=False)
-
-    def clean_name(self):
-        name = self.cleaned_data.get('name')
-        name = name.title()
-        return name
-
-    def clean(self):
-        if not self.cleaned_data.get('email') and not self.cleaned_data.get('phone'):
-            raise ValidationError("informe e-mail ou Telefone.")
-
-        return self.cleaned_data
 
 
 class SubscriptionForm(forms.ModelForm):
@@ -40,9 +22,3 @@ class SubscriptionForm(forms.ModelForm):
             raise ValidationError("informe e-mail ou Telefone.")
 
         return self.cleaned_data
-
-# def save(self):
-    #     instance = super().save(commit=False)
-    #     instance.cpf_hash = hash(instance.get('cpf'))
-    #     instance.save()
-    #     return instance
